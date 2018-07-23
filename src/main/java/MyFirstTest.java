920bbce2ba42fa94a0fbdb6314748faa66f9ac57
sdvic <gvw@san.rr.com>
2018-07-23 10:47:49 -0700

    Successfully iterate through local volunteers

--- src/main/java/MyFirstTest.java
+++ src/main/java/MyFirstTest.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MyFirstTest
@@ -17,7 +19,7 @@ public class MyFirstTest
         {
             webDriver.findElement(By.name("name")).sendKeys("vic.wintriss@scorevolunteer.org");
             System.out.println("User Name Found/Sent");
-            webDriver.findElement(By.name("pass")).sendKeys("xxxxxxxxxxxx");
+            webDriver.findElement(By.name("pass")).sendKeys("xxxxxxxx");
             System.out.println("Password Found/Sent");
             webDriver.findElement(By.id("edit-submit")).click();
             System.out.println("Click On LOG IN");
@@ -27,6 +29,20 @@ public class MyFirstTest
             System.out.println("Found CORE and clicked it");
             webDriver.findElement(By.xpath("//a[text()=\"Manage Chapter Info & Volunteers\"]")).click();
             System.out.println("Found Volunteers and clicked it");
+            webDriver.findElement(By.xpath("//a[text()=\"Volunteers (91)\"]")).click();
+            System.out.println("Volunteers (91) and clicked");
+//            webDriver.findElement(By.xpath("//a[text()=\"John Batt\"]")).click();
+//            System.out.println("Found John Batt and clicked");
+            List<WebElement> drop = webDriver.findElements(By.className("oddListRowS1"));
+            java.util.Iterator<WebElement> i = drop.iterator();
+            while(i.hasNext()) {
+                WebElement row = i.next();
+                System.out.println(row.getText());
+            }
+
+
+
+
         }
         catch (Exception e)
         {
