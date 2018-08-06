8e6601e08d49824075399219001ede08b4798869
keithagroves <keith.groves@jointheleague.org>
2018-08-05 19:29:24 -0700

    Update MyFirstTest.java

--- src/main/java/MyFirstTest.java
+++ src/main/java/MyFirstTest.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-public class MyFirstTest
+public class MyFirstTest throws FileNotFoundException
 {
     private static int i;
 
